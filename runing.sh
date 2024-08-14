4bf6d2b381310e6f2d3ebe386911c34b426e09a4
HusoNode <154274674+aksamlan@users.noreply.github.com>
2024-08-14 03:55:31 +0300

    Create runing.sh

---
+++ runing.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Önceki cüzdan mnemonic kelimeleri giriniz
+read -p "Enter your mnemonic phrase: " mnemonic_phrase
+
+# Repoyu Clonluyoruz
+git clone https://github.com/allora-network/basic-coin-prediction-node
+
+# Dizini değiştir
+cd basic-coin-prediction-node
+
+# JSON içeriğini config.json'a yazın
+cat <<EOF > config.json
+{
+  "wallet": {
+    "addressKeyName": "test",
+    "addressRestoreMnemonic": "$mnemonic_phrase",
+    "alloraHomeDir": "",
+    "gas": "1000000",
+    "gasAdjustment": 1.0,
+    "nodeRpc": "https://sentries-rpc.testnet-1.testnet.allora.network/",
+    "maxRetries": 1,
+    "delay": 1,
+    "submitTx": false
+  },
+  "worker": [
+    {
+      "topicId": 1,
+      "inferenceEntrypointName": "api-worker-reputer",
+      "loopSeconds": 5,
+      "parameters": {
+        "InferenceEndpoint": "http://inference:8000/inference/{Token}",
+        "Token": "ETH"
+      }
+    },
+    {
+      "topicId": 2,
+      "inferenceEntrypointName": "api-worker-reputer",
+      "loopSeconds": 5,
+      "parameters": {
+        "InferenceEndpoint": "http://inference:8000/inference/{Token}",
+        "Token": "ETH"
+      }
+    }
+  ]
+}
+EOF
+
+echo "Belirtilen içerikle oluşturulan yapılandırma dosyası."
+
+mkdir worker-data
+chmod +x init.config
+./init.config 
+
+# Docker containers calistiralim
+docker-compose up -d
